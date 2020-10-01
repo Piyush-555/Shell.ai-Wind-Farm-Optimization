ae3895759733bec66d3da45a1f5f1a3724aff17d
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-10-02 01:58:39 +0530

    Add scipy optimization setup

---
+++ gradient_free/main_scipy.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+import pandas as pd
+import scipy
+from scipy import optimize
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+import Farm_Evaluator_Vec as evaluator
+
+
+def get_constraints():
+    constraints = []
+    for i in range(50):
+        for j in range(i+1, 50):
+            def constraint(params):
+                turb_params = params.reshape((50, 2))
+                return np.linalg.norm(turb_params[i] - turb_params[j]) - 0.1
+            constraints.append({'type': 'ineq', 'fun': constraint})
+    return constraints
+
+
+def objective(params):
+    turb_coords = param2coords(params)
+    turb_rad = 50
+
+    power_curve = evaluator.loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
+    n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = evaluator.preProcessing(power_curve)
+
+    aeps = []
+    wind_dir = "Shell_Hackathon Dataset/Wind Data"
+    for file in os.listdir(wind_dir):
+        wind_inst_freq =  evaluator.binWindResourceData(os.path.join(wind_dir, file))
+        AEP = evaluator.getAEP(turb_rad, turb_coords, power_curve, wind_inst_freq, 
+                     n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t)
+        aeps.append(AEP)
+
+    return -np.mean(aeps)/500
+
+
+def coords2param(turb_coords):
+    return turb_coords.flatten() / 4000
+
+
+def param2coords(params):
+    return params.reshape((50, 2)) * 4000
+
+
+def save_params(params):
+    coords = param2coords(params)
+    df = pd.DataFrame({
+        'x': coords[:, 0],
+        'y': coords[:, 1]
+    })
+    df.to_csv('Submissions/SLSQPv1.csv', index=False)
+
+
+if __name__ == "__main__":
+    num_iter = 1500
+
+    path = "Submissions/cma_540.73.csv"
+    turb_coords = evaluator.getTurbLoc(path)
+    params0 = coords2param(turb_coords)
+    bounds = [(0.0125, 0.9875)] * 100
+    cnstr = get_constraints()
+    result = optimize.minimize(objective, params0, method='SLSQP', bounds=bounds, constraints=cnstr, options={'finite_diff_rel_step': '3-point'})
+    print(result)
+    save_params(result.x)
+    coords = param2coords(result.x)
+    plt.scatter(coords[:, 0], coords[:, 1])
+    plt.show()
+
--- gradient_free/plot_turbines.py
+++ gradient_free/plot_turbines.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import Farm_Evaluator_Vec as evaluator
 
-path = "Submissions/fixed_2000.csv"
+path = "Submissions/SLSQPv0.csv"
 turb_coords = evaluator.getTurbLoc(path)
 x, y = turb_coords[:, 0], turb_coords[:, 1]
 plt.scatter(x, y)
