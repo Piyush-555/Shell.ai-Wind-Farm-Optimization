6531c652250530ce28a5138a3aab7c9d16a8c1d1
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-10-01 17:08:13 +0530

    Add smooth penalty Remove BoundaryHandler

--- gradient_free/main.py
+++ gradient_free/main.py
@@ -10,7 +10,7 @@ import cma
 import Farm_Evaluator_Vec as evaluator
 
 
-def get_constraints_penalty(turb_coords, p_perimeter, p_proximity, verbose=False, turb_diam=100):
+def get_constraints_penalty(turb_coords, verbose=False, turb_diam=100):
     """
     p_perimeter: Penalty for each perimeter constraint violation.
     p_proximity: Penalty for each proximity constraint violation.
@@ -20,6 +20,8 @@ def get_constraints_penalty(turb_coords, p_perimeter, p_proximity, verbose=False
     bound_clrnc      = 50
     prox_constr_viol = 0
     peri_constr_viol = 0
+    prox_penalty = 0
+    peri_penalty = 0
     
     # create a shapely polygon object of the wind farm
     farm_peri = [(0, 0), (0, 4000), (4000, 4000), (4000, 0)]
@@ -27,30 +29,32 @@ def get_constraints_penalty(turb_coords, p_perimeter, p_proximity, verbose=False
     
     # checks if for every turbine perimeter constraint is satisfied. 
     # increments peri_constr_viol if violated.
+    # add penalty to peri_penalty.
     for turb in turb_coords:
         turb = Point(turb)
         inside_farm   = farm_poly.contains(turb)
         correct_clrnc = farm_poly.boundary.distance(turb) >= bound_clrnc
         if (inside_farm == False or correct_clrnc == False):
             peri_constr_viol += 1
+            peri_penalty += 20 - 1/5 * farm_poly.boundary.distance(turb) * (1 if inside_farm else -1)
     
     # checks if for every turbines proximity constraint is satisfied. 
     # increments prox_constr_viol if violated.
+    # add penalty to peri_penalty.
     for i,turb1 in enumerate(turb_coords):
         for turb2 in turb_coords[i+1:]:
-            if  np.linalg.norm(turb1 - turb2) < 4*turb_diam:
+            if  np.linalg.norm(turb1 - turb2) < 4 * turb_diam:
                 prox_constr_viol += 1
+                prox_penalty += 20 - 1/40 * np.linalg.norm(turb1 - turb2)
     
     if verbose:
         print("Perimeter Constraints Violated:", peri_constr_viol, "\tProximity Constraints Violated:", prox_constr_viol)
 
-    return -(p_perimeter * peri_constr_viol + p_proximity * prox_constr_viol)
+    return -(peri_penalty + prox_penalty)
 
 
 def evaluate_coords(turb_coords):
     turb_rad = 50
-    p_perimeter = 10
-    p_proximity = 0
 
     power_curve = evaluator.loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
     n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = evaluator.preProcessing(power_curve)
@@ -64,7 +68,7 @@ def evaluate_coords(turb_coords):
         aeps.append(AEP)
 
     mean_aep = np.mean(aeps)
-    penalty = get_constraints_penalty(turb_coords, p_perimeter, p_proximity, verbose=False)
+    penalty = get_constraints_penalty(turb_coords, verbose=False)
     fitness = mean_aep + penalty
 
     return fitness, penalty
@@ -88,22 +92,20 @@ def save_params(params):
 
 
 if __name__ == "__main__":
-    num_gen = 3000
+    num_gen = 1500
     population_size = 40
-    patience = 3
-    patience_length = 10
+    # patience = 3
+    # patience_length = 10
 
     path = "Submissions/fixed_2000.csv"
     turb_coords = evaluator.getTurbLoc(path)
 
-    es = cma.CMAEvolutionStrategy(coords2param(turb_coords), 0.167, {
-        'BoundaryHandler': 'BoundPenalty',
-        'bounds': [0.0125, 0.9875]
-    })
+    es = cma.CMAEvolutionStrategy(coords2param(turb_coords), 0.167)
     # queue = [0,]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    max_fitness = -float('inf')
     for gen in range(1, num_gen + 1):
         solutions = es.ask(population_size)
 
@@ -115,14 +117,19 @@ if __name__ == "__main__":
         function_values = -returns[:, 0]  # cma-es minimizes the objective
 
         best_idx = function_values.argmin()  # argmin to find the most positive fitness value
+        best_coords = param2coords(solutions[best_idx])
+        best_fitness, _ = returns[best_idx]
         print("Generation:", gen)
+        print("Generation Mean -> Mean_fitness: {}\t Penalty: {}".format(*returns.mean(axis=0)))
         print("Generation Best -> Mean_fitness: {}\t Penalty: {}".format(*returns[best_idx]))
-        print("Generation Mean -> Mean_fitness: {}\t Penalty: {}\n".format(*returns.mean(axis=0)))
+        get_constraints_penalty(best_coords, verbose=True)
+        print()
 
-        save_params(solutions[best_idx])
-        coords = param2coords(solutions[best_idx])
+        if best_fitness > max_fitness:
+            save_params(solutions[best_idx])
+            max_fitness = best_fitness
         
-        ax.scatter(coords[:, 0], coords[:, 1])
+        ax.scatter(best_coords[:, 0], best_coords[:, 1])
         fig.canvas.draw()
         plt.pause(1e-6)
         ax.clear()
